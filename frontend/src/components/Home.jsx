a11a3af44fa0fada826ebe60f8393d0983a94d7d
MireX420 <adrian_mirek@yahoo.no>
2024-04-30 14:06:25 +0200

    HTML struktur til header og home

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,12 @@
+import Header from "./Header"
+export default function Home(){
+    return(
+        <>
+        <Header/>
+        <main>
+            <h2>MAIN POKEMONS</h2>
+            <h3>TYPES</h3>
+        </main>
+      </>
+    )
+}
