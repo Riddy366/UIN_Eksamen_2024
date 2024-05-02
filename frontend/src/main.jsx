ab1c075fa162dad0ceeee12c00f63b8b1318f8a2
abdalla162 <abdalla162@hotmail.com>
2024-05-02 14:28:40 +0200

    Oppdateringer på Type komponenten

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createRoot } from 'react-dom/client'
+import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+const container = document.getElementById('root')
+
+
+const root = createRoot(container)
+
+
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
