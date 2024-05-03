62b195481d3958cdd07beb5242cf0744edab362e
Riddy366 <ridwanabukar851@gmail.com>
2024-05-03 14:23:08 +0200

    Enkel HTML struktur for Pokemon kort

---
+++ frontend/src/components/PokeCard.jsx
@@ -0,0 +1,20 @@
+export default function PokeCard(){
+    return(
+    <article>
+        <h1>POKEMON-NAME</h1>
+        <img src="bulbasaur.png" alt="POKEMON-NAME"/>
+        <h2>TYPE(S)</h2>
+        <p>POKEMON-TYPE</p>
+        <h2>STATS</h2>
+        <p>Hp</p>
+        <p>Attack</p>
+
+        <h2>ABILITIES</h2>
+        <h3>ABILITY 1</h3>
+        <p>DESCRIPTION</p>
+        <h3>ABILITY 2</h3>
+        <p>DESCRIPTION</p>
+    </article>
+    )
+    
+}
