1a99cea9418a18acaf33a833de550b4874940a6e
MireX420 <adrian_mirek@yahoo.no>
2024-05-10 14:15:42 +0200

    rydding opp i merging av home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,11 @@
-import { useState } from 'react'
-
 import Home from './components/Home'
-
 import './App.css'
-import { SearchBar } from './components/SearchBar'
+
 
 function App() {
 
   return (
-      <div className="App">
         <Home/>
-     
-        <section className="search-bar-container">
-          <SearchBar />
-          <h1>SearchResults</h1>
-        </section>
-      </div>
   )
 }
 
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,10 +1,12 @@
+import { SearchBar } from "./SearchBar";
+
 export default function Header(){
     return(
         <>
         <header>
             <h3>UIN POKEDEX</h3>
             <h3>TEAMS</h3>
-            <input type="text"/>
+            <SearchBar />
         </header>
         </>
     )
