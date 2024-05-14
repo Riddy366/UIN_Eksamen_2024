de082df544de6ff066748863b90b50cd09046b3e
MireX420 <adrian_mirek@yahoo.no>
2024-05-14 12:12:42 +0200

    startet på en løsning til pokemon visning

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@
 import Home from './components/Home'
 import Type from './components/Type'
 import Teams from './components/Teams'
+import Pokemon from './components/Pokemon'
 import './App.css'
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
@@ -13,6 +14,7 @@ function App() {
     <>
         <Routes>
           <Route index element={<Home />} />
+            <Route path="id" element={<Pokemon />} />
           <Route path="type" element={<Type/>} />
           <Route path="teams" element={<Teams/>} />
         </Routes>
