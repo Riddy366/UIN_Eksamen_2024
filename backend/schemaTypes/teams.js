0ac122b81e2aba610c8c0ef6647412ea9df92049
Riddy366 <ridwanabukar851@gmail.com>
2024-05-18 19:39:32 +0200

    setter opp datastrukturen til sanity studio

---
+++ backend/schemaTypes/teams.js
@@ -0,0 +1,23 @@
+export const team = {
+    title: "Teams",
+    name: "teams",
+    type: "object",
+    fields: [
+        {
+            title: "Pokemon Team",
+            name: "teams",
+            type: "string"
+        },
+        {
+            title: "Pokemons",
+            name: "pokemons",
+            type: "string"
+        },
+        {
+            title: "bilde",
+            name: "image",
+            type: "image"
+        }
+        
+    ]
+}
