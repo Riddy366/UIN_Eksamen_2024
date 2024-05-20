d284a11b673d9b79b6ced418664a8e5a199e9c51
Riddy366 <ridwanabukar851@gmail.com>
2024-05-20 20:38:58 +0200

    la tilbake html for teams

--- frontend/src/components/Teams.jsx
+++ frontend/src/components/Teams.jsx
@@ -1,7 +1,36 @@
-export default function Teams(){
-  return(
-      <>
-          <h2>TEAMS</h2>
-      </>
-  )
-}
+import React, { useEffect, useState } from 'react';
+import { fetchTeams } from '../../sanity/services/queries';
+
+const Teams = () => {
+  const [teams, setTeams] = useState([]);
+
+  useEffect(() => {
+    const getTeams = async () => {
+      const data = await fetchTeams();
+      setTeams(data);
+    };
+
+    getTeams();
+  }, []);
+
+  return (
+    <div>
+      <h1>Teams</h1>
+      {teams.map((team) => (
+        <div key={team._id}>
+          <h2>{team.teamName}</h2>
+          <ul>
+            {team.pokemons.map((pokemon, index) => (
+              <li key={index}>{pokemon}</li>
+            ))}
+          </ul>
+          {team.image && (
+            <img src={team.image.asset.url} alt={team.teamName} style={{ width: '200px' }} />
+          )}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Teams;
