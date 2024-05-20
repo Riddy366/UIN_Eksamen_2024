673e2191d7b33e2dd74a3c2771227d9beca8a1f5
abdalla162 <abdalla162@hotmail.com>
2024-05-20 19:47:24 +0200

    Update på type, fikk lagt inn sånn at bildene kommer opp

--- frontend/src/components/Type.jsx
+++ frontend/src/components/Type.jsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import "../styles/Type.css";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+import "../styles/Type.css"
+import { Link } from "react-router-dom"
 
 export default function Type() {
   const { type } = useParams()
@@ -12,34 +12,49 @@ export default function Type() {
   }
   //https://codedamn.com/news/javascript/how-to-capitalize-first-letter-in-javascript
 
-
-  useEffect(() => {
+//Henter pokemon apiene, tok maks på 20 pokemons.
+useEffect(() => {
     const fetchPokemonType = async () => {
       try {
-        const response = await fetch(`https://pokeapi.co/api/v2/type/${type}`)
-        const data = await response.json()
-        setPokemonType(data.pokemon.map(p => p.pokemon))
+        const response = await fetch(`https://pokeapi.co/api/v2/type/${type}`);
+        const data = await response.json();
+        const detailedPokemon = await Promise.all(
+          data.pokemon.slice(0, 20).map(async (p) => {
+            const pokemonResponse = await fetch(p.pokemon.url)
+            const pokemonData = await pokemonResponse.json()
+            return {
+              name: pokemonData.name,
+              image: pokemonData.sprites.front_default,
+              id: pokemonData.id
+            };
+          })
+        );
+        setPokemonType(detailedPokemon);
+
       } catch (error) {
-        console.error('Error fetching Pokemon by type:', error)
+        console.error('Error fetching Pokemon by type:', error);
       }
+
     }
     fetchPokemonType()
   }, [type])
 
   return (
     <>
-      <h1>{type.toUpperCase()}</h1>
+      <h1>{type ? type.toUpperCase() : ''}</h1>
       <section className="pokemonList">
-        {pokemonType.slice(0,20).map((poke, index) => (
-          <article key={poke.name} className="pokemon">
-            <Link key={index} to={`/Pokemons/${poke.name}`}>
-              <h3>{capitalizeFirstLetter(poke.name)}</h3>
+        {pokemonType.map(poke => (
+          <article key={poke.id} className="pokemon">
+            <Link to={`/pokemons/${poke.name}`}>
+              <img src={poke.image} alt={poke.name} />
+              <div>
+                <h3>{capitalizeFirstLetter(poke.name)}</h3>
+                <span>#{poke.id}</span>
+              </div>
             </Link>
           </article>
         ))}
       </section>
     </>
-  )
+  );
 }
-
-
