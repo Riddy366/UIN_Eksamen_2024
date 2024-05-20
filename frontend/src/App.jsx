93b1bbe4301817b3997ec5bdb42bd4912a5d62b6
Riddy366 <ridwanabukar851@gmail.com>
2024-05-20 20:39:30 +0200

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,27 @@
+import Home from './components/Home'
+import Type from './components/Type'
+import Teams from './components/Teams'
+import Pokemon from './components/Pokemon'
+import SearchResult from './components/SearchResult'
+import './App.css'
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
-import Type from './components/Type';
-import Teams from './components/Teams';
-import Pokemon from './components/Pokemon';
-import SearchResult from './components/SearchResult';
-import Layout from './components/Layout';
-import './App.css';
+import Layout from './components/Layout'
 
+
+//Lagde routing
 function App() {
-  return (
-    <Router>
+    return (
       <Layout>
         <Routes>
           <Route index element={<Home />} />
-          <Route path="pokemons/:pokemonName" element={<Pokemon />} />
-          <Route path="/:type" element={<Type />} />
-          <Route path="teams" element={<Teams />} />
-          <Route path="searchresults/:query" element={<SearchResult />} />
+            <Route path="pokemons/:pokemonName" element={<Pokemon />} />
+            <Route path="/:type" element={<Type />} />
+            <Route path="teams" element={<Teams/>} />
+            <Route path="searchresults/:query" element={<SearchResult />} />
         </Routes>
       </Layout>
-    </Router>
-  );
+    )
 }
 
 export default App;
