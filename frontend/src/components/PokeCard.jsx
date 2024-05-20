0270a467148040472c5cfc0e4b8211857a57779b
Riddy366 <ridwanabukar851@gmail.com>
2024-05-20 13:01:19 +0200

    Mapping av pokemonkott

--- frontend/src/components/PokeCard.jsx
+++ frontend/src/components/PokeCard.jsx
@@ -1,20 +1,24 @@
-export default function PokeCard(){
-    return(
-    <article>
-        <h1>POKEMON-NAME</h1>
-        <img src="bulbasaur.png" alt="POKEMON-NAME"/>
-        <h2>TYPE(S)</h2>
-        <p>POKEMON-TYPE</p>
-        <h2>STATS</h2>
-        <p>Hp</p>
-        <p>Attack</p>
+import React from 'react';
 
-        <h2>ABILITIES</h2>
-        <h3>ABILITY 1</h3>
-        <p>DESCRIPTION</p>
-        <h3>ABILITY 2</h3>
-        <p>DESCRIPTION</p>
-    </article>
-    )
-    
+// brukt https://github.com/PokeAPI/sprites# for å finne ut hvordan bildet skal displayes
+export default function PokeCard({ pokemon }) {
+    return (
+        <article>
+            <h1>{pokemon.name.toUpperCase()}</h1>
+            <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+            <h2>TYPE(S)</h2>
+            <p>{pokemon.types.map(type => type.type.name).join(', ')}</p>
+            <h2>STATS</h2>
+            {pokemon.stats.map(stat => (
+                <p key={stat.stat.name}>{stat.stat.name}: {stat.base_stat}</p>
+            ))}
+            <h2>ABILITIES</h2>
+            {pokemon.abilities.map((ability, index) => (
+                <div key={ability.ability.name}>
+                    <h3>ABILITY {index + 1}</h3>
+                    <p>{ability.ability.name}</p>
+                </div>
+            ))}
+        </article>
+    );
 }
